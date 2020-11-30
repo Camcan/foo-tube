738edd4e57ad7f19ad0ff0a73c88fc42be599e87
Campbell Hawkes <mr.campbell.hawkes@gmail.com>
2020-11-30 18:59:08 +1300

    Initial video upload form

--- client/views/upload/UploadVideoForm.js
+++ client/views/upload/UploadVideoForm.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import useStyles from '../../hooks/useStyles';
+import { ENDPOINTS } from '../../consts/api';
+import FileUpload from '../../components/input/FileUpload';
 
 export default function UploadVideoForm() {
 	const classes = useStyles(styles);
+	const formRef = useRef();
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		const data = new FormData(formRef.current);
+		fetch(ENDPOINTS.uploadVideo, { method: 'POST', body: data })
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
+
+	const onFileSelect = (file) => console.log('file', file);
 
 	return (
 		<section css={classes.root}>
 			Upload Video Form
+			<form ref={formRef} onSubmit={onSubmit}>
+				<label>
+					<p>Title:</p>
+					<input type="text" name="title" required />
+				</label>
+				<label>
+					<p>Video file:</p>
+					<FileUpload onFileSelect={onFileSelect} name="video_file" required />
+				</label>
+				<button type="submit">Upload</button>
+			</form>
 		</section>
 	);
 }
